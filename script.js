787f61141069eb9cafcf7d73684a15fa7d8726f4
Daephne <92085466+Daephne@users.noreply.github.com>
2024-04-20 10:44:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const slide = ["https://drive.google.com/thumbnail?id=1fJkQyc3HcQ1-tlbPuq4KzDZIvSGLKCbG", "https://drive.google.com/thumbnail?id=1M2fcywvc-5Pz9kRB9rL1MJ_tTQlEIMyh", "https://drive.google.com/thumbnail?id=1fJkQyc3HcQ1-tlbPuq4KzDZIvSGLKCbG"];
+let numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
+
+
+
+
 function chargerCSV(chemin) {
     fetch(chemin)
         .then(response => response.text())
